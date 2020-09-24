96335774576b9010c37c2eb65bd68f4715606a56
Amit Gupta <iiita.amitgupta@gmail.com>
2020-09-24 10:47:06 +0530

    Running Successfully; Controller Completed; Output: Gives empty json file([]) as data is not added, by visiting Link(http://localhost:3000/api/v1/users)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,52 @@
 class Api::V1::UsersController < ApplicationController
+    #GET /users
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    #GET /user/:id
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    #Post /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render error: {error: 'Unable to create User.' }, status: 400
+        end
+    end
+
+    #PUT /users/:id
+    def update
+        @user = User.find(params[:id])
+        if @user
+            @user.update(user_params)
+            render json: { message: 'User Successfully Updated.' }, status: 200
+        else
+            render json: {error: 'Unable to update User.' }, status:400
+        end
+    end
+
+    # DELETE /users/:id
+    def destroy
+        @user = User.find(params[:id])
+        if @user
+            @user.destroy
+            render json: { message: 'User successfully deleted.' }, status: 200
+        else
+            render json: { error: 'Unable to delete User.' }, ststus: 400
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
